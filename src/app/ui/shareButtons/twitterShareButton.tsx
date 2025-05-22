a71e174db83edc0d626f22894c3fde1539f07c4e
kofrah <kofrah20@gmail.com>
2025-05-22 20:38:20 +0900

    fix X icon to svg

--- src/app/ui/shareButtons/twitterShareButton.tsx
+++ src/app/ui/shareButtons/twitterShareButton.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import ShareButtonBase from "./shareButtonBase";
 
 type Props = {
@@ -26,7 +25,19 @@ export default function TwitterShareButton({
     <ShareButtonBase
       href={shareUrl}
       icon={
-        <Image src="/x-logo-white.png" alt="Xロゴ" width={18} height={18} />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 1227"
+          width="18"
+          height="18"
+          fill="white"
+          className="drop-shadow-sm"
+        >
+          <path
+            transform="translate(0, 2)"
+            d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
+          />
+        </svg>
       }
       label="ポスト"
       bgColor="bg-black"
